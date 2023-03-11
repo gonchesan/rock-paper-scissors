97a3156ee86dba44c9d65f547591b43cb914e916
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2023-03-11 00:05:47 -0300

    Add Ribbon Component

---
+++ src/components/Ribbon/index.tsx
@@ -0,0 +1,18 @@
+import { RESULTS } from "@/constants";
+import { RibbonProps } from "@/models/Ribbon";
+
+import "./styles.scss";
+const Ribbon: React.FC<RibbonProps> = ({ result }) => {
+  return (
+    <div className={`ribbon${result === "win" ? "--won" : ""}`}>
+      <i></i>
+      <i></i>
+      <i></i>
+      <i></i>
+      <span className="ribbon-top"></span>
+      <p> {RESULTS[result]}</p>
+    </div>
+  );
+};
+
+export default Ribbon;
